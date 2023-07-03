c762d6c713e15ceda0f383956e8f4ddc90228472
Ryan Folks <47860863+RyanDoesMath@users.noreply.github.com>
2023-07-03 13:07:31 -0400

    Fixed docstring. Imported only whats needed from cv2.

--- src/deshadow.py
+++ src/deshadow.py
@@ -1,6 +1,6 @@
 """The deshadow module provides a function for deshadowing and normalizing and image."""
 
-import cv2
+from cv2 import dilate, medianBlur, absdiff, normalize, NORM_MINMAX, CV_8UC1
 import numpy as np
 
 
@@ -8,22 +8,22 @@ def deshadow_and_normalize_image(image: np.ndarray):
     """Removes shadows from an image and normalizes it.
 
     Args:
-        filepath:str - the filepath to the image.
+        image:np.ndarray - a cv2 image.
 
     Returns: A deshadowed, normalized image.
     """
     if not isinstance(image, np.ndarray):
         raise TypeError("Image is not a cv2 image.")
-    dilated_img = cv2.dilate(im, np.ones((7, 7), np.uint8))
-    bg_img = cv2.medianBlur(dilated_img, 21)
-    diff_img = 255 - cv2.absdiff(im, bg_img)
+    dilated_img = dilate(image, np.ones((7, 7), np.uint8))
+    medblur_img = medianBlur(dilated_img, 21)
+    diff_img = 255 - absdiff(image, medblur_img)
     norm_img = diff_img.copy()  # Needed for 3.x compatibility
-    norm_img = cv2.normalize(
+    norm_img = normalize(
         diff_img,
         norm_img,
         alpha=0,
         beta=255,
-        norm_type=cv2.NORM_MINMAX,
-        dtype=cv2.CV_8UC1,
+        norm_type=NORM_MINMAX,
+        dtype=CV_8UC1,
     )
     return norm_img
