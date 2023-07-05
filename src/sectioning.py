f64ba2dea22742487f54f4d790b71340a7970eba
Ryan Folks <47860863+RyanDoesMath@users.noreply.github.com>
2023-07-05 09:28:32 -0400

    Added package imports.

--- src/sectioning.py
+++ src/sectioning.py
@@ -1,2 +1,5 @@
 """The sectioning module contains methods for identifying and cropping the
 individual sections for other modules to be able to use."""
+
+from ultralytics import YOLO
+from PIL import Image
