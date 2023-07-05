79c9847a9de8ffdc3d1e7a6412c0bf5965ccfef5
Ryan Folks <47860863+RyanDoesMath@users.noreply.github.com>
2023-07-05 09:42:57 -0400

    Stubbed out & wrote docstring for helper functions.

--- src/sectioning.py
+++ src/sectioning.py
@@ -3,6 +3,7 @@ individual sections for other modules to be able to use."""
 
 from ultralytics import YOLO
 from PIL import Image
+import pandas as pd
 
 SECTIONING_MODEL = YOLO("../models/section_cropper_yolov8.pt")
 
@@ -25,5 +26,31 @@ def extract_sections(image) -> dict:
             'checkboxes'
         ]
         and PIL image values corresponding to the key section.
+    """
+    preds = SECTIONING_MODEL(image, verbose=False)
+    preds_df = make_predictions_into_dataframe(preds)
+    preds_df = filter_section_predictions(preds)
+    for box in preds[0].boxes:
+        pass
+
+
+def make_predictions_into_dataframe(preds):
+    """Converts YOLOv8 predictions into a dataframe.
+
+    Parameters:
+        preds - a YOLOv8 predictions object.
+
+    Returns : the predictions in a pandas dataframe.
+    """
+    pass
+
+
+def filter_section_predictions(preds_df):
+    """Chooses the prediction with higher confidence if there are duplicates.
+
+    Parameters:
+        preds_df - a dataframe with the section predictions.
+
+    Returns : a filtered dataframe with a unique row per predicted section.
     """
     pass
