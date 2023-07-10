5cf9dc417be2e5f0678b858decdaa2a2c14b748b
Ryan Folks <47860863+RyanDoesMath@users.noreply.github.com>
2023-07-10 10:07:08 -0400

    Stubbed out tile_predict and tile_image

--- src/tiles.py
+++ src/tiles.py
@@ -3,3 +3,24 @@ YOLOv8 models to predict on sets of tiles, and defines
 functions that automatically filter and combine tiles with
 predictions, allowing other code which uses tiles to not
 worry about the implementation of image tiling."""
+
+from typing import List
+
+
+def tile_predict(
+    model, image, rows: int, columns: int, stride: float = 1 / 2
+) -> List[List[float]]:
+    """Uses a YOLOv8 model to predict on an image using image tiling."""
+
+
+def tile_image(image, rows: int, columns: int, stride: float = 1 / 2) -> List[List]:
+    """Tiles an image.
+
+    Args :
+        image : the PIL image to be tiled.
+        rows : the number of rows that the image will be cut into.
+        columns : the number of columns that the image will be cut into.
+        stride : the amount that the window should slide when making cuts.
+
+    Returns : A tiled image, where you can find the tile you wish by tiles[row][col].
+    """
