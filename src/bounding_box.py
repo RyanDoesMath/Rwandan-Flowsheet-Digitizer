54f0a2c0e34b2c8e7dcb3382e9f6b8225bdd419a
Ryan Folks <47860863+RyanDoesMath@users.noreply.github.com>
2023-07-19 14:03:21 -0400

    Changed BoundingBox to a dataclass.
    Because why not?

--- src/bounding_box.py
+++ src/bounding_box.py
@@ -1,27 +1,24 @@
 """Defines the BoundingBox class."""
 
+from dataclasses import dataclass
 
+
+@dataclass
 class BoundingBox:
     """Defines and gives methods for a bounding box.
 
     Uses the PIL convention of having y=0 be the top of the image.
     """
 
-    def __init__(self, left: int, top: int, right: int, bottom: int):
-        """Inits the BoundingBox."""
-        self.left = left
-        self.top = top
-        self.right = right
-        self.bottom = bottom
-        self.width = right - left
-        self.height = bottom - top
-        self.x_center = self.compute_x_center()
-        self.y_center = self.compute_y_center()
+    left: int
+    top: int
+    right: int
+    bottom: int
 
-    def compute_x_center(self) -> float:
-        """Computes the x center of the box."""
-        return self.left + self.width / 2
-
-    def compute_y_center(self) -> float:
-        """Computes the y center of the box."""
-        return self.top + self.height / 2
+    def __post_init__(self):
+        """Inits variables that depend on the four constructor variables."""
+        self.width = self.right - self.left
+        self.height = self.bottom - self.top
+        self.x_center = self.left + (self.width / 2)
+        self.y_center = self.top + (self.height / 2)
+        self.area = (self.right - self.left) * (self.bottom - self.top)
