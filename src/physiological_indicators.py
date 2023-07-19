5fd75ff38954edf2eb812ee2e6dac69a674a4a58
Ryan Folks <47860863+RyanDoesMath@users.noreply.github.com>
2023-07-19 13:36:45 -0400

    Added dataclasses for the rows of the physiological indicators section.

--- src/physiological_indicators.py
+++ src/physiological_indicators.py
@@ -1,2 +1,61 @@
-"""The physiological_indicators module extracts the data from the 
+"""The physiological_indicators module extracts the data from the
 physiological indicator section of the Rwandan flowsheet using YOLOv8."""
+
+from dataclasses import dataclass
+
+
+@dataclass
+class OxygenSaturation:
+    """Dataclass for spo2."""
+
+    percent: int
+    timestamp: int
+
+
+@dataclass
+class EndTidalCarbonDioxide:
+    """Dataclass for etco2."""
+
+    percent: int
+    timestamp: int
+
+
+@dataclass
+class FractionOfInspiredOxygen:
+    """Dataclass for fio2."""
+
+    percent: int
+    timestamp: int
+
+
+@dataclass
+class TidalVolume:
+    """Dataclass for Tidal Vol x F."""
+
+    tidal_volume_ml: int
+    respiratory_rate: int
+    timestamp: int
+
+
+@dataclass
+class Temperature:
+    """Dataclass for temp in celcius."""
+
+    temp_c: int
+    timestamp: int
+
+
+@dataclass
+class Diuresis:
+    """Dataclass for diuresis (urine output)."""
+
+    diuresis_ml: int
+    timestamp: int
+
+
+@dataclass
+class BloodLoss:
+    """Dataclass for estimated blood loss."""
+
+    blood_loss_ml: int
+    timestamp: int
