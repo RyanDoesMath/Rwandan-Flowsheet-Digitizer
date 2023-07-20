9a80d7ba9bdfdf8ccc6e5a9ddee49b17dfac242f
Ryan Folks <47860863+RyanDoesMath@users.noreply.github.com>
2023-07-20 11:32:13 -0400

    Create oxygen_saturation module and added a module docstring.

---
+++ src/oxygen_saturation.py
@@ -0,0 +1,3 @@
+"""The oxygen_saturation module provides an implementation for 
+get_values_for_boxes that the physiological_indicators module can
+use to get values for the bounding box detections it made."""
