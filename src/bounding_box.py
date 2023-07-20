1f942ca5249d6dbab421ce1987b562983443d3bd
Ryan Folks <47860863+RyanDoesMath@users.noreply.github.com>
2023-07-20 09:15:43 -0400

    Added IOU computation to the BoundingBox class.

--- src/bounding_box.py
+++ src/bounding_box.py
@@ -24,3 +24,21 @@ class BoundingBox:
         self.x_center = self.left + (self.width / 2)
         self.y_center = self.top + (self.height / 2)
         self.area = (self.right - self.left) * (self.bottom - self.top)
+
+    def intersection_over_union(self, other: BoundingBox) -> float:
+        """Computes the intersection over union of the two bounding boxes."""
+        smaller, _ = (self, other) if self.area < other.area else (other, self)
+        intersection_left, intersection_right = max((self.left, other.left)), min(
+            (self.right, other.right)
+        )
+        intersection_top, intersection_bottom = max((self.top, other.top)), min(
+            (self.bottom, other.bottom)
+        )
+        width, height = (
+            intersection_right - intersection_left,
+            intersection_bottom - intersection_top,
+        )
+        intersection_area = width * height
+        if width > 0 and height > 0:
+            return intersection_area / smaller.area
+        return 0
