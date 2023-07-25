891ea9c8435c49d043817d479a6c538de02b8ca6
Ryan Folks <47860863+RyanDoesMath@users.noreply.github.com>
2023-07-25 09:52:36 -0400

    Finished updating BoundingBox to make only function calls for computed data.

--- src/bounding_box.py
+++ src/bounding_box.py
@@ -33,14 +33,17 @@ class BoundingBox:
             -1,
             -1,
         )
-        if intersection.width > 0 and intersection.height > 0:
-            return intersection.area / (self.area + other.area - intersection.area)
+        width = intersection.get_width()
+        height = intersection.get_height()
+        area = intersection.get_area()
+        if width > 0 and height > 0:
+            return area / (self.get_area() + other.get_area() - area)
         return 0
 
     def intersection_over_smaller_box(self, other) -> float:
         """Computes the intersection over the area of the smaller box of the two
         bounding boxes."""
-        smaller = self if self.area < other.area else other
+        smaller = self if self.get_area() < other.get_area() else other
         intersection_left, intersection_right = max((self.left, other.left)), min(
             (self.right, other.right)
         )
@@ -55,8 +58,11 @@ class BoundingBox:
             -1,
             -1,
         )
-        if intersection.width > 0 and intersection.height > 0:
-            return intersection.area / smaller.area
+        width = intersection.get_width()
+        height = intersection.get_height()
+        area = intersection.get_area()
+        if width > 0 and height > 0:
+            return area / smaller.get_area()
         return 0
 
     def get_box(self):
@@ -65,11 +71,11 @@ class BoundingBox:
 
     def get_x_center(self):
         """Gets the x center for the box."""
-        return self.left + (self.width / 2)
+        return self.left + (self.get_width() / 2)
 
     def get_y_center(self):
         """Gets the y center for the box."""
-        return self.top + (self.height / 2)
+        return self.top + (self.get_height() / 2)
 
     def get_width(self):
         """Gets the width for the box."""
