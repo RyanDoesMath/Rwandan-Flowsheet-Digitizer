9eade61ee046e3c94d88131155cdc8692274cb49
Ryan Folks <47860863+RyanDoesMath@users.noreply.github.com>
2023-07-27 10:30:22 -0400

    Stubbed out get_limits_for_number_of_clusters

--- src/oxygen_saturation.py
+++ src/oxygen_saturation.py
@@ -2,6 +2,10 @@
 needs to have erroneous values dealt with, as well as which values are plausible."""
 
 from dataclasses import dataclass
+from typing import Tuple
+
+LOWEST_PLAUSIBLE_VALUE = 75
+HIGHEST_PLAUSIBLE_VALUE = 100
 
 
 @dataclass
@@ -13,3 +17,14 @@ class OxygenSaturation:
     percent: int
     timestamp: int
     implausible: bool = False
+
+
+def get_limits_for_number_of_clusters(number_of_boxes: int) -> Tuple(int):
+    """Gets the upper and lower limit for the number of clusters that could be made with the
+    number of boxes present.
+
+    Args :
+        number_of_boxes - the number of boxes detected.
+
+    Returns - a tuple containing the upper and lower limits of the number of clusters.
+    """
