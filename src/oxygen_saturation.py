7140f2cf5bdf65ff8f406c2373ff429b016a9b92
Ryan Folks <47860863+RyanDoesMath@users.noreply.github.com>
2023-07-27 10:34:16 -0400

    Implemented and improved documentation for get_limits_for_number_of_clusters

--- src/oxygen_saturation.py
+++ src/oxygen_saturation.py
@@ -23,8 +23,16 @@ def get_limits_for_number_of_clusters(number_of_boxes: int) -> Tuple(int):
     """Gets the upper and lower limit for the number of clusters that could be made with the
     number of boxes present.
 
+    The lower limit for oxygen saturation is if every entry is 100, with a 20% allowance for
+    extraneous boxes that are overdetections by the YOLO model, and the upper limit is if there
+    are no 100 entries, again with a 20% allowance for missed boxes that the YOLO model did not
+    find.
+
     Args :
         number_of_boxes - the number of boxes detected.
 
     Returns - a tuple containing the upper and lower limits of the number of clusters.
     """
+    lower_lim = number_of_boxes // 3 - int(0.2 * number_of_boxes // 3)
+    upper_lim = number_of_boxes // 2 + int(0.2 * number_of_boxes // 2)
+    return lower_lim, upper_lim
