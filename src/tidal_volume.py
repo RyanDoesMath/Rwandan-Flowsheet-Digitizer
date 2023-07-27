575159e58f0b976cad2fa4e28fce40e38c653cdf
Ryan Folks <47860863+RyanDoesMath@users.noreply.github.com>
2023-07-27 15:49:37 -0400

    Added the tidal_volume dataclass.

--- src/tidal_volume.py
+++ src/tidal_volume.py
@@ -1,2 +1,16 @@
 """Provides the strategy for get_values_for_boxes to physiological_indicators for the
 tidal volume row of the physiological indicators."""
+
+from dataclasses import dataclass
+
+
+@dataclass
+class TidalVolume:
+    """Dataclass for tidal volume."""
+
+    chars: list
+    boxes: list
+    respiratory_rate: int
+    tidal_volume: int
+    timestamp: int
+    implausible: bool = False
