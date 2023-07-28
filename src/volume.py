5b31f24fe5c4b15f55c8b61a5b4637600226c610
Ryan Folks <47860863+RyanDoesMath@users.noreply.github.com>
2023-07-28 11:51:43 -0400

    Created volume.py and added the module docstring.

---
+++ src/volume.py
@@ -0,0 +1,3 @@
+"""Provides strategies for the volume half of the tidal volume x respiratory rate section of the
+physiological indicators which relate specifically to how the volume half of that section needs to
+have erroneous values dealt with, as well as which values are plausible."""
